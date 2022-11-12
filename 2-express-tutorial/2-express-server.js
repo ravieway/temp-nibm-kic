ed231e9fb695b4806c3ab9ed6af35a7336a505aa
ravieway <ravibs.mail@gmail.com>
2022-11-12 15:38:24 +0530

    day4

---
+++ 2-express-tutorial/2-express-server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const app = express()
+app.get('/',(req,res)=>{
+    res.status(200).send('Home Page')
+})
+
+app.get('/about',(req,res)=>{
+    res.status(200).send('About Page')
+})
+
+app.all('*',(req,res)=>{
+    res.status(404).send('<h1>Not Found</h1>')
+})
+
+app.listen(5000,()=>{
+    console.log('server is listening on port 5000')
+})
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use
+//app.listen
